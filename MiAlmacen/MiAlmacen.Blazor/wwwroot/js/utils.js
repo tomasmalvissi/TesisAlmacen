d241ddfe6a6d41bf215106f071e51467a392dd76
Mauricio Gaggero <mauricio.gaggero@hotmail.com>
2021-09-02 18:17:31 -0300

    add mensaje sweetalert js

---
+++ MiAlmacen/MiAlmacen.Blazor/wwwroot/js/utils.js
@@ -0,0 +1,26 @@
+﻿function confirmar(title, text, icon) {
+    return new Promise(resolve => {
+        Swal.fire({
+            title,
+            text,
+            icon,
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            cancelButtonText: 'Cancelar',
+            confirmButtonText: 'Aceptar'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                resolve(result.isConfirmed);
+            }
+        })
+    })
+}
+
+function simple(title, text, icon) {
+    Swal.fire({
+        title,
+        text,
+        icon
+    })
+}
