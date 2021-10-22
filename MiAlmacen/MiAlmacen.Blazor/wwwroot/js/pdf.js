3db05876fe360fc044d270c802a1b41ec89222a7
Mauricio Gaggero <mauricio.gaggero@hotmail.com>
2021-10-21 22:08:20 -0300

    descarga de facturas en pdf

---
+++ MiAlmacen/MiAlmacen.Blazor/wwwroot/js/pdf.js
@@ -0,0 +1,13 @@
+﻿function generarPdf(id) {
+
+    var element = document.getElementById(id);
+    var opt = {
+        margin: 1,
+        filename: 'INVOICE.pdf',
+        image: { type: 'jpeg', quality: 0.98 },
+        html2canvas: { scale: 2, logging: true, dpi: 192, letterRendering: true },
+        jsPDF: { unit: 'mm', format: 'a4', orientation: 'portrait' }
+    };
+
+    html2pdf().set(opt).from(element).save();
+}
